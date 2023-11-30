22133f03f8ebdb261e55f407f4748377e6d3b875
Kiril Raykov <kirilraykov@gmail.com>
2023-12-01 01:14:31 +0200

    Initialize empty Spring Project

---
+++ src/main/java/com/kraykov/emerchantapp/EMerchantAppApplication.java
@@ -0,0 +1,13 @@
+package com.kraykov.emerchantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMerchantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMerchantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kraykov/emerchantapp/EMerchantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kraykov.emerchantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMerchantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
